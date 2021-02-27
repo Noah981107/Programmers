1dbaf2b942e699b9cc6fe27490dd203177b6b909
Noah <76483681+Noah981107@users.noreply.github.com>
2021-02-27 13:20:47 +0900

    가운데 글자 가져오기
    가운데 글자 가져오기

---
+++ Level1/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        if(len%2 !=0){
+            answer = Character.toString(s.charAt((len-1)/2));
+        }
+        else{
+            answer = Character.toString((s.charAt((len/2)-1)))+ Character.toString(s.charAt(len/2));
+        }
+        return answer;
+    }
+}
