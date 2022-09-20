3d72c1ad57bd274b5d2fd4cb40cf26079424782d
Noah <shgustmd6865@naver.com>
2022-09-20 10:32:37 +0900

    n^2 배열 자르기

---
+++ Level2/n^2 배열 자르기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left) + 1];
+
+        for (int i = 0; i < answer.length; i++) {
+            int max = Math.max((int)((left + i) / n), (int)((left + i) % n));
+            answer[i] = max + 1;
+        }
+
+        return answer;
+    }
+}
